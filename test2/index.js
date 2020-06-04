d09dd2453f86ad6a3fee3f5766ec2689d0b32344
AshkanAD <ashkan.a.dastgerdi@gmail.com>
2020-06-04 12:03:20 +0100

    testing elm parcel builds

---
+++ test2/index.js
@@ -0,0 +1,5 @@
+const root = require("./src/Example1.elm");
+
+const app = root.Elm.Example1.init({
+    node: document.getElementById('elm-node')
+});
---
+++ test2/src/Example1.elm
@@ -0,0 +1,14 @@
+module Example1 exposing (main)
+
+import Browser exposing (..)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+
+
+
+-- MAIN
+
+
+main =
+    text "Hello world!"
