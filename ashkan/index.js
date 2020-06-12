1bc3ba3dc63fcd05f182de162b12bce726dcec27
AshkanAD <ashkan.a.dastgerdi@gmail.com>
2020-06-12 15:40:37 +0100

    adding wordguess

--- ashkan/index.js
+++
@@ -1,5 +0,0 @@
-const root = require("./Main.elm");
-
-const app = root.Elm.Main.init({
-    node: document.getElementById('elm-node')
-});
--- test2/src/Example1.elm
+++ test2/src/Example1.elm
@@ -1,7 +1,7 @@
-module Example1 exposing (main)
+module Example1 exposing (Model, Msg(..), init, main, update, view)
 
-import Browser exposing (..)
-import Html exposing (..)
+import Browser
+import Html exposing (Html, div, input, text)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
 
@@ -11,4 +11,45 @@ import Html.Events exposing (onInput)
 
 
 main =
-    text "Hello world!"
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { content : String
+    }
+
+
+init : Model
+init =
+    { content = "" }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Change String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Change newContent ->
+            { model | content = newContent }
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ input [ placeholder "Text to reverse", value model.content, onInput Change ] []
+        , div [] [ text (String.reverse model.content) ]
+        ]
